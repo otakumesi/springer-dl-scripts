d98c62c70a65e1f76fb2af439f1a003aeb7c5b35
Takuro Niitsuma <bakednt@gmail.com>
2020-04-12 15:23:21 +0900

    add dl log

--- dl_from_springer.py
+++ dl_from_springer.py
@@ -1,9 +1,10 @@
-import ipdb
 from time import sleep
+from glob import glob
+
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from glob import glob
+from tqdm import tqdm
 
 SPRINGER_URL = 'https://link.springer.com'
 EBOOK_LIST_EXCEL_DIR = './src'
@@ -17,6 +18,9 @@ def main():
     excel_path = [f for f in files if f.endswith('.xlsx')][0]
     urls = read_ebook_list(excel_path)[EBOOK_URL_COLUMN_NAME]
 
+    bar = tqdm(total=len(urls))
+    bar.set_description('Downloaded')
+
     for url in urls:
         text = get_content(url)
         soup = BeautifulSoup(text, 'lxml')
@@ -24,11 +28,16 @@ def main():
         title = soup.find('h1').text
         pdf_url = soup.find('a', class_='test-bookpdf-link')['href']
 
+        print(f'\nStart Download: {title}\n')
         pdf = get_content(f'{SPRINGER_URL}/{pdf_url}')
 
         with open(f'{DOWNLOADED_PDF_DIR}/{title}.pdf', 'wb') as f:
             f.write(pdf)
+        print(f'End Download: {title}\n')
+
+        bar.update(1)
         sleep(2)
+    bar.close()
 
 
 
@@ -36,7 +45,7 @@ def read_ebook_list(file_path):
     return pd.read_excel(file_path, EBOOK_LIST_SHEET_INDEX)
 
 def get_content(url):
-    r = requests.get(url, timeout=5)
+    r = requests.get(url)
 
     if r.status_code != 200:
         return None
